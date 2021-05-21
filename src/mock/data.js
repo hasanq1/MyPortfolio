dd0a54eeb9e11ef77ad240c53f6d5ef18f4440f7
hasanq1 <44348396+hasanq1@users.noreply.github.com>
2021-05-21 14:40:08 -0500

    Add files via upload

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,7 +4,7 @@ import { nanoid } from 'nanoid';
 export const headData = {
   title: 'Hasan Qureshi', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: 'Welcome to my website', // e.g: Welcome to my website
+  description: 'Welcome to my online portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -19,11 +19,13 @@ export const heroData = {
 export const aboutData = {
   img: 'IMG_0052.JPG',
   paragraphOne:
-    "Currently, a senior at the University of Houston. Studying to earn my Bachelor's in Computer Science with a focus on software engineering. Programming and web dev is my passion. macOS is my favorite operating system and pretty much anything  creates. I enjoy meeting new people to collaborate with and am always on the lookout for new projects to work on. My philosophy is to never stop learning; so naturally, I enjoy stepping outside of my comfort zone to learn new skills, even if it’s not tech-related.",
+    "Currently a senior at the University of Houston. I am studying to earn my Bachelor's in Computer Science with focus on software engineering, networking, and cyber security. Programming and web dev is my passion. macOS is my favorite operating system and pretty much anything that’s  . I enjoy meeting new people to collaborate with and am always on the lookout for new projects to work on.",
   paragraphTwo:
-    'I usually travel from my former residence in Brooklyn, NY to my current one in Houston, Texas, and am open to opportunities in either city. Some of my other aspersions, are to be an Officer in the United States Air Force and eventually start my own cybersecurity company."',
-  paragraphThree: '"Your time is limited, so don’t waste it living someone else’s life."',
-   // if no resume, the button will not show up
+    'My philosophy is to never stop learning; so naturally, I enjoy stepping outside of my comfort zone to learn new skills - even if it’s not tech-related. I usually travel from my former residence in Brooklyn, NY to my current one in Houston, Texas, and am open to opportunities in either city. Some of my aspersions are to be an Officer in the United States Air Force, and eventually start my own tech start-up.',
+
+  paragraphThree:
+    '"Favorite quotes: “Stay hungry, stay foolish” “Have the courage to follow your heart and intuition. They somehow already know what you truly want to become. Everything else is secondary.” “Your time is limited, so don’t waste it living someone else’s life.” " Amat Victoria Curam ""',
+  // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -33,9 +35,9 @@ export const projectsData = [
     img: 'coding.jpeg',
     title: 'Programming',
     info:
-      "One of my passions is to create new programs. I' written many programs from class projects and also build some programs on the side for fun. I also love web development and enjoy working with the react framework and node for the backend.",
+      "I love coding. I' written many programs in class, and also have some projects. I also love web development and enjoy working with the react library for the frontend and node for the backend. My favorite stack to work with is the MERN stack",
     info2:
-      'My favorite operating system is macOS because of how versatile and secure the OS is. The possibilities for me on macOS is endless, afterall this website was created on a MacBook Pro. Click Source code to see some of my github projects.',
+      'My favorite operating system is macOS because of its versatility and security. The creativity never stops for me on macOS-- afterall this website was created on my MacBook Pro. Here are some other projects I have created on my MacBook.',
     url: '',
     repo: 'https://github.com/hasanq1', // if no repo, the button will not show up
   },
@@ -44,7 +46,7 @@ export const projectsData = [
     img: 'nyc.jpg',
     title: 'Photograpy',
     info:
-      "I enjoy photography too, I have a collection of stills that I use as my personal wallpapaers. I havent posted many but I just love metropolitan scenes and capturing that point in time as a still life. Here is one photo I've taken with my iPhone's amazing camera and currently have as my wallpaper ",
+      "I enjoy photography too, I have a collection of still lifes that I use as my personal wallpapaers. I havent posted many but I just love metropolitan scenes and capturing various moments in time, it is as they say 'we take pictures to go back in time and revisit old moments'. Here is one photo I've taken with my iPhone's amazing camera which I also have as my wallpaper ",
     info2: '',
     url: '',
     repo: '', // if no repo, the button will not show up
@@ -53,7 +55,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'project.jpg',
     title: 'Traveling',
-    info: 'I love traveling to new cities in the US. I mainly travel back and forth from my residence in NYC and Houston. Ive also been to 7/50 states. Only 43 left to go and eventually Mexico and South America as well. ',
+    info:
+      'I love traveling to new cities in the US. I mainly travel back and forth from my residence in NYC and Houston. Ive also been to 7/50 states. Only 43 left to go, and eventually Mexico and South America as well. ',
     info2: '',
     url: '',
     repo: '', // if no repo, the button will not show up
